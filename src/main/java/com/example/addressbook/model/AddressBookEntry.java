554d015271298d7d570369cc0a93995408e8cc32
prince356 <princechaurasia014@gmail.com>
2025-03-21 15:32:55 +0530

    Added UC2 - DTO and Model for Address Book

---
+++ src/main/java/com/example/addressbook/model/AddressBookEntry.java
@@ -0,0 +1,56 @@
+package com.example.addressbook.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "address_book")
+public class AddressBookEntry {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String phoneNumber;
+    private String email;
+
+    public AddressBookEntry() {}
+
+    public AddressBookEntry(String name, String phoneNumber, String email) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
